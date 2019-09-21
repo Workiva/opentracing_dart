e9bef39b0b912a808c6d62ea3147ccc71dc4c668
Tod Bachman <tod.bachman@workiva.com>
2019-09-21 08:11:39 -0600

    CPLAT-7488 Run Dart 2 unit tests on dart2js

--- tool/test.sh
+++ tool/test.sh
@@ -2,15 +2,14 @@
 
 # Fast fail the script on failures.
 set -e
+set -x
 
 DART_VERSION=$(dart --version 2>&1)
 DART_2_PREFIX="Dart VM version: 2"
 
 
 if [[ $DART_VERSION = $DART_2_PREFIX* ]]; then
-    echo -e 'pub run build_runner test -- -p vm -p chrome'
-    pub run build_runner test -- -p vm -p chrome
+    pub run build_runner test --release -- -p chrome
 else
-    echo -e 'pub run test -p vm -p chrome'
     pub run test -p vm -p chrome
 fi
